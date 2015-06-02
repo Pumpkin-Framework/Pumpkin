4cde8ccf57701b63a4206db5f80a678264e40451
jk-5 <jeffreykog@me.com>
2015-06-02 10:16:18 +0200

    Removed Pumpkin-Hooks submodule and gradle module stuff

--- src/main/java/nl/jk_5/pumpkin/PumpkinPlugin.java
+++ src/main/java/nl/jk_5/pumpkin/PumpkinPlugin.java
@@ -8,18 +8,9 @@ import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.entity.player.PlayerChatEvent;
-import org.spongepowered.api.event.entity.player.PlayerJoinEvent;
-import org.spongepowered.api.event.entity.player.PlayerRespawnEvent;
-import org.spongepowered.api.event.server.StatusPingEvent;
 import org.spongepowered.api.event.state.ServerAboutToStartEvent;
-import org.spongepowered.api.event.world.WorldCreateEvent;
-import org.spongepowered.api.event.world.WorldLoadEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.permission.PermissionService;
-import org.spongepowered.api.text.Texts;
-import org.spongepowered.api.world.DimensionTypes;
-import org.spongepowered.api.world.GeneratorTypes;
-import org.spongepowered.api.world.difficulty.Difficulties;
 
 import nl.jk_5.pumpkin.command.PumpkinCommands;
 import nl.jk_5.pumpkin.hooks.PumpkinHooksService;
@@ -47,39 +38,6 @@ public class PumpkinPlugin {
             logger.info("Pumpkin Hooks service was not found");
             throw new RuntimeException("Pumpkin Hooks service missing");
         }
-    }
-
-    @Subscribe
-    public void onWorldCreate(WorldCreateEvent event){
-        logger.info("World " + event.getWorldCreationSettings().getWorldName() + " is being created");
-        event.getWorldProperties().setGeneratorType(GeneratorTypes.FLAT);
-        event.getWorldProperties().setDifficulty(Difficulties.PEACEFUL);
-        if(event.getWorldCreationSettings().getDimensionType() != DimensionTypes.OVERWORLD){
-            event.getWorldProperties().setKeepSpawnLoaded(false);
-            event.getWorldProperties().setLoadOnStartup(false);
-        }
-    }
-
-    @Subscribe
-    public void onWorldLoad(WorldLoadEvent event){
-
-    }
-
-    @Subscribe
-    public void onPlayerJoin(PlayerJoinEvent event){
-        //event.setLocation(world2.getSpawnLocation());
-    }
-
-    @Subscribe
-    public void onRespawn(PlayerRespawnEvent event){
-        event.setRespawnLocation(game.getServer().getWorld("world").get().getSpawnLocation());
-    }
-
-    @Subscribe
-    public void onMultiplayerStatus(StatusPingEvent event){
-        String input = event.getClient().getVirtualHost().get().getHostString();
-        event.getResponse().setDescription(Texts.of(input));
-        event.getResponse().getPlayers().get().setMax(9001);
     }
 
     @Subscribe
